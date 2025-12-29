c462e588a37a5d2f38596229d6074e70a004afd2
Circuit-Overtime <ayushbhatt633@gmail.com>
2025-12-30 00:39:27 +0530

    moved all .JS to be inside the utils

--- blogs.elixpo/src/pages/AboutPage.jsx
+++ blogs.elixpo/src/pages/AboutPage.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import '../../CSS/about/about.css'
+import '../styles/about/about.css'
 
 export default function AboutPage() {
-  return (
-    <div className="page-container about-page">
-      <h1>About Page</h1>
-      <p>This is the about page. Information about the application will be here.</p>
-    </div>
-  );
+    document.title = 'About - LixBlogs';
+    return (
+        <div className="container absolute flex flex-col h-full max-w-[2560px] bg-[#030712] box-border">
+            <div className="appHeader">
+                <div className="logo"></div>
+                <p className="appName">LixBlogs</p>
+            </div>
+        </div>
+    );
 }
