73c2cc17a6854c5557469ccafadca44f0e300c5f
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-02-07 11:48:54 +0530

    removing back the blogs.elixpo pre subtree

--- blogs.elixpo/api/config.js
+++
@@ -1,3 +0,0 @@
-const MAX_EXPIRE_TIME = 10 * 60 * 1000; 
-
-export default MAX_EXPIRE_TIME;
--- blogs.elixpo/src/pages/auth/register/RegisterPage.jsx
+++
@@ -1,8 +0,0 @@
-export default function RegisterPage() {
-  return (
-    <div className="page-container register-page">
-      <h1>Register Page</h1>
-      <p>This is the register page. New users can create an account here.</p>
-    </div>
-  );
-}
