15d7ad6139a8bd737cffb1ba6fe50d8dd54a63b7
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-02-08 22:47:45 +0530

    added a layout and ui refactor to allow google and github buttons

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,14 @@
+import Navbar from '@/app/components/navbar';
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
 }
